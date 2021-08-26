c966115e390e85ece777787b33b4060056cb3578
Robbie Case <Robbie@Robbies-MacBook-Pro.local>
2021-08-26 10:43:13 -0500

    Implement Sum of Squares

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def sum_of_squares(a):
-	pass
+	sum = 0
+	for i in a:
+		sum += i*i;
+	print sum
+
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
+
+def test_two():
+    assert sum_of_squares([2,3,4]) == 29
